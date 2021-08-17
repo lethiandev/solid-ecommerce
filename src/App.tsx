05d1cfccb3c6e45acbdd142ef254436a09a9d593
Konrad Nowakowski <konrad.x92@gmail.com>
2021-08-17 15:30:25 +0100

    Add tailwindcss styling framework

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import styles from "./App.module.css"
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
+    <div class="text-center">
+      <header class="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          class={styles.link}
+          class="text-purple-500"
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"
