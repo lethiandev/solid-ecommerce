8ecf6ba0d3ded5c1d1257cdd4e75517244cb5e6f
Konrad Nowakowski <konrad.x92@gmail.com>
2021-08-29 15:11:37 +0100

    Fix base path resolving

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: isProduction ? '/solid-ecommerce/' : '/',
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
