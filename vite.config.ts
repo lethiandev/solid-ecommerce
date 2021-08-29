abb248df36afaa52dd89304d31b8038fe4d144fc
Konrad Nowakowski <konrad.x92@gmail.com>
2021-08-29 15:22:22 +0100

    Fix code formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production"
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: isProduction ? '/solid-ecommerce/' : '/',
+  base: isProduction ? "/solid-ecommerce/" : "/",
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
